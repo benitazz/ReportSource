da799865e1a89f664733f8167557be6c0d252a79
benitazz <benitazz@gmail.com>
2014-08-23 21:27:31 +0200

    new class

---
+++ ExcelReportsGenerator/Common/TestClass.cs
@@ -0,0 +1,17 @@
+﻿namespace ExcelReportsGenerator.Common
+{
+  /// <summary>
+  /// The test class.
+  /// </summary>
+  public class TestClass
+  {
+    #region Public Properties
+
+    /// <summary>
+    /// Gets or sets the name.
+    /// </summary>
+    public string Name { get; set; }
+
+    #endregion
+  }
+}
